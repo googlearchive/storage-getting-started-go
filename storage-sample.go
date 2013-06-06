dd88c702324e5a9fc9edc23e9f596271f2ae6b5b
zwilt <zwilt@google.com>
2013-06-05 23:44:29 -0600

    Update storage-sample.go

--- storage-sample.go
+++ storage-sample.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2013 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 // Binary storage-sample creates a new bucket, performs all of its operations
 // within that bucket, and then cleans up after itself if nothing fails along the way.
 package main
@@ -28,6 +44,7 @@ const (
 	authURL    = "https://accounts.google.com/o/oauth2/auth"
 	tokenURL   = "https://accounts.google.com/o/oauth2/token"
 	entityName = "allUsers"
+	redirectURL = "urn:ietf:wg:oauth:2.0:oob"
 )
 
 var (
@@ -45,6 +62,7 @@ var (
 		AuthURL:      authURL,
 		TokenURL:     tokenURL,
 		TokenCache:   oauth.CacheFile(*cacheFile),
+		RedirectURL:  redirectURL,
 	}
 )
 
